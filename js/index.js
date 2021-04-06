6a3f8f1c2665bf89c0a05234efab8320d7cdaeb4
Chad Cook <Charles.Cook28@gmail.com>
2021-04-06 02:49:59 -0500

    uploading js file with an array of links to projects

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// var headContainer = $("#headContainer");
+
+// var intElemClientHeight = headContainer.clientHeight;
+// var intElemClientWidth = headContainer.clientWidth;
+// var intViewportHeight = window.innerHeight;
+// var intVeiwportWidth = window.innerWidth;
+
+// var bkgdDiv = $("<div>");
+// var bkgdDivRow = headContainer.append(bkgdDiv) * 10;
+// // var bkgdDivRow = bkgdDivApp.repeat(10);
+
+// var row = setInterval(appendRow, 200);
+
+// bkgdDiv.addClass("bkgdDiv");
+
+// function appendRow() {
+//     headContainer.append(bkgdDivRow);
+    // if (headContainer.clientHeight > window.innerHeight) {
+    //     clearInterval(row);
+// }
+// }
+
+
+
+// const myName = ["C", "h", "a", "d"];
+
+// var i = 0;
+
+// var id = setInterval(addText, 500); 
+
+// function addText() {
+//         // for (var i = 0; i < myName.length; i++) {
+//             document.getElementById("test").innerText += myName[i];
+//             i++
+//             if (i > myName.length - 1) {
+//                 clearInterval(id);
+//             }            
+//     }
+
